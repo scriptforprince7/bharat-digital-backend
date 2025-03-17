fda2b815da7b85654ecacd0f7a29c2a9077449bd
Prince Sachdeva <scriptforprince@gmail.com>
2025-03-18 00:17:06 +0530

    Initial commit for django and node js patchup server request

---
+++ ecommerce_node/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const orderRoutes = require('./routes/orderRoutes');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/api', orderRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Node server running on port ${PORT}`));
